47eb5eada2dcc180d7668db708e2bedb64a6be0e
Milad <mimalakooti@gmail.com>
2020-02-12 15:58:23 -0800

    updates the game played

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-let reels=['cherry', 'flower', 'popcorn', 'candle', 'socks',
-'lemon','dollar','nerd','tooth','wink'];
+let reels = ['cherry', 'flower', 'popcorn', 'candle', 'socks',
+    'lemon', 'dollar', 'nerd', 'tooth', 'wink'];
 
 let emojiCodes = {   // names and emojies
     cherry: "🍒",
@@ -13,9 +13,9 @@ let emojiCodes = {   // names and emojies
     tooth: "🦷",
     wink: "😉"
 }
-
-function rand(){
-    return reels[Math.floor(Math.random()*reels.length)];
+let arr
+function rand() {
+    return reels[Math.floor(Math.random() * reels.length)];
 }
 
 let emojies = document.getElementById("emojies")
@@ -24,20 +24,23 @@ let middleBox = document.getElementById("middleBox")
 let rightBox = document.getElementById("rightBox")
 
 //
-let timesPlayed=-1;
-function appendEls(){
+let timesPlayed = -1;
+function appendEls() {
+
+    
     slot1 = rand()
     slot2 = rand()
     slot3 = rand()
 
-    
-    
-    let arr = [slot1,slot2,slot3]
-    console.log(slot1,slot2,slot3)
-    
-    let boxes =[leftBox, middleBox, rightBox]   
-        
-    for (let i=0; i<arr.length;i++){
+
+
+    arr = [slot1, slot2, slot3]
+    console.log(slot1, slot2, slot3)
+
+    let boxes = [leftBox, middleBox, rightBox]
+
+    for (let i = 0; i < arr.length; i++) {
+        boxes[i].innerHTML = "" 
         let div = document.createElement("DIV")
         div.setAttribute("id", arr[i])
         div.setAttribute("class", "emoji")
@@ -46,24 +49,25 @@ function appendEls(){
     }
 
     win()
-       
-    increaseTimesPlayed(); 
+
+    increaseTimesPlayed();
     console.log(timesPlayed);
 }
 
 
 document.getElementById("beginButton").addEventListener("click", appendEls)
-    
-appendEls()
-    
 
-function win(){
+
+appendEls()
+
+
+function win() {
     // console.log('You won!')
-    if (slot1 == slot2 && slot2==slot3){
+    if (slot1 == slot2 && slot2 == slot3) {
         console.log('Jackpot!')
-    } else if (slot1==slot2 || slot2==slot3 || slot1==slot3){
-        console.log('Two matches') 
     }
+     else if (slot1==slot2 || slot2==slot3 || slot1==slot3){
+        console.log('Two matches') }
 }
 
 function increaseTimesPlayed() {
@@ -75,7 +79,7 @@ console.log(timesPlayed);
 
 
 // function removePlayed(){
-    
+
 // }
 
 // function myFunction() {
